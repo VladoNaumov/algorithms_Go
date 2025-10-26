1c6042be77273238a96c349a8839ff0a506d2744
umbre <umbrella.spb@hotmail.com>
2025-10-26 15:26:46 +0200

    commit

--- ch01/main.go
+++ ch01/main.go
@@ -1,8 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func doOuter(a, b int) int {
+	return a + b
+}
+
+func doMain(theIterac int) int {
+	start := time.Now() // Запоминаем время начала
+
+	theArr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	theRes := 0
+
+	for theIterac > 0 {
+		theIterac--
+		sum := 0
+		for i, v := range theArr {
+			if i == 0 {
+				sum = v
+			} else {
+				sum = doOuter(sum, v)
+			}
+		}
+		theRes = sum
+	}
+
+	elapsed := time.Since(start) // Вычисляем время выполнения
+	fmt.Printf("Код выполнялся %s\n", elapsed)
+
+	return theRes
+}
 
 func main() {
-	fmt.Println("Im Go")
-
+	fmt.Println("Результат:", doMain(1_000_000_000))
 }

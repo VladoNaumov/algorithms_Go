b862b194f2438c127fe07686a9526f65db691ffc
umbre <umbrella.spb@hotmail.com>
2025-10-30 20:07:04 +0200

    Missed payment

--- debug/main.go
+++ debug/main.go
@@ -6,52 +6,20 @@ import (
 	"fmt"
 )
 
-// 1. Максимальная прибыль (одна сделка), prices{7, 1, 5, 3, 6, 4}, prices {7, 6, 4, 3, 1}
-func maxProfitOneTransaction(prices []int) int {
-	if len(prices) < 2 {
-		return 0
+// 5. Пропущенный платёж (числа 1..N, одно пропущено)
+func findMissingPayment(payments []int) int {
+	n := len(payments) + 1 // поскольку одно пропущено
+	sumExpected := n * (n + 1) / 2
+	sumActual := 0
+	for _, v := range payments {
+		sumActual += v
 	}
-	minPrice := prices[0]
-	maxProfit := 0
-	for _, price := range prices[1:] {
-		if price < minPrice {
-			minPrice = price
-		} else {
-			if profit := price - minPrice; profit > maxProfit {
-				maxProfit = profit
-			}
-		}
-	}
-	return maxProfit
-}
-
-// 2. Анализ баланса (Кадане) — максимальная сумма подмассива
-func maxSubarraySum(changes []int) int {
-	maxSoFar := changes[0]
-	currentMax := changes[0]
-	for i := 1; i < len(changes); i++ {
-		if currentMax < 0 {
-			currentMax = changes[i]
-		} else {
-			currentMax += changes[i]
-		}
-		if currentMax > maxSoFar {
-			maxSoFar = currentMax
-		}
-	}
-	return maxSoFar
+	return sumExpected - sumActual
 }
 
 func main() {
-	// 1
-	fmt.Println("\n1)  Максимальная прибыль (одна сделка):")
-	fmt.Println(maxProfitOneTransaction([]int{7, 1, 5, 3, 6, 4})) // ожидает 5
-	fmt.Println(maxProfitOneTransaction([]int{7, 6, 4, 3, 1}))    // ожидает 0
-	fmt.Println(maxProfitOneTransaction([]int{3, 1, 20, 3, 6, 1, 13}))
-
-	// 2
-	fmt.Println("\n2) Анализ баланса (Кадане):")
-	fmt.Println(maxSubarraySum([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})) // ожидает 6 (4,-1,2,1)
-	fmt.Println(maxSubarraySum([]int{1, -2, -3, -4}))                 // ожидает 1
-
+	// 5
+	fmt.Println("\n5) findMissingPayment:")
+	fmt.Println(findMissingPayment([]int{3, 1, 4, 2, 6, 5}))
+	fmt.Println(findMissingPayment([]int{1, 2, 3, 5, 6, 7}))
 }
